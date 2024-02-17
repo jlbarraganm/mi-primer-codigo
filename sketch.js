7a9fe52ae630f4beca858b843c0ccf40f043c9ee
jlbarraganm <160335263+jlbarraganm@users.noreply.github.com>
2024-02-17 14:27:46 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(255,180,0);
+  circle(300, 200, 300);
+  rect(300, 125, 132, 7);
+  rect(165, 265, 130, 7);
+  circle(300, 200, 150);
+  circle(300, 200, 130);
+}
